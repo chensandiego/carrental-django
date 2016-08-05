30b594ca238545a59ee00e28aae3705e6638c93e
chen <chensandiego@gmail.com>
2016-08-05 13:36:13 +0800

    final

---
+++ frontend/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from frontend.models import Car
+from frontend.models import Booking
+
+
+class BookingModelAdmin(admin.ModelAdmin):
+	list_display=['customer_name','booking_start_date','is_approved']
+	list_filter=['is_approved']
+	list_editable=['is_approved']
+	search_fields=['customer_name']
+
+	actions=['email_customers']
+
+	def email_customers(self,request,queryset):
+		for booking in queryset:
+			if booking.is_approved:
+				email_body="""Dear {},
+				we are pleased to inform you that your booking has been approved.
+				Thanks
+				""".format(booking.customer_name)
+			else:
+				email_body=""" Dear {},
+				Unfortunately we do not have the capacity right now to accept your booking.
+				Thanks""".format(booking.customer_name)
+
+			print(email_body)
+
+		self.messaeg_user(request,'emails were send successfully')
+	email_customers.short_description='Send email about booing status to customers'
+admin.site.register(Car)
+
+admin.site.register(Booking,BookingModelAdmin)
+# Register your models here.
